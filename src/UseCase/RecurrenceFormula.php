ccac859e0b4eb01032a9b81b07d1abcf6bfca775
yosky1011 <yosky1011@gmail.com>
2023-08-23 22:40:50 +0900

    Adds ナップザック問題

--- src/UseCase/RecurrenceFormula.php
+++ src/UseCase/RecurrenceFormula.php
@@ -49,11 +49,48 @@ class RecurrenceFormula
         return $dp;
     }
 
-    /**
-     *
-     */
-    public function dynamicPlanNapzac()
-    {
 
+    /**
+     * @param int $N 商品数
+     * @param int $W 重さの許容重量
+     * @param array<int> $wList 「重さ」が格納された配列 
+     * @param array<int> $vList 「価値」が格納された配列 
+     * @return int 重さが許容量に収まっている商品の価値合計の最大値
+     */
+    public function dynamicPlanNapzac(int $N, int $W, array $wList, array $vList): int
+    {
+        $dp = [];
+        for ($i = 0; $i <= $N; $i ++)
+        {
+            for ($j = 0; $j <= $W; $j ++)
+            {
+                // 各数値を0で初期化する
+                $dp[$i][$j] = 0;
+
+                // 何も選んでいない状態なのでスキップする
+                if ($i == 0) continue;
+
+                // 「重さ」が2次元テーブルの「重さカラム」より小さかったら、一段上のレコードの同じ重さカラムの「価値」を代入
+                if ($j < $wList[$i-1]) $dp[$i][$j] = $dp[$i-1][$j];
+
+                // 「重さ」が「重さカラム」以上だったら、
+                if ($j >= $wList[$i-1]) 
+                {
+                    // 「一段上のレコード」の同じ「重さカラム」の価値
+                    $v1 = $dp[$i - 1][$j];
+
+                    // 「一段上のレコード」の「自分の重さを引いた重さカラム」の価値に「自分の価値」を足した価値
+                    $v2 = $dp[$i - 1][$j - $wList[$i-1]] + $vList[$i-1];
+
+                    $dp[$i][$j] = max($v1, $v2);
+                }
+            }
+        }
+
+        // 2次元配列の一番最後の配列の中で一番大きな数値を返却する
+        return max($dp[$N]);
     }
 }
+
+
+
--- tests/UseCase/RecurrenceFormulaTest.php
+++ tests/UseCase/RecurrenceFormulaTest.php
@@ -47,10 +47,25 @@ class RecurrenceFormulaTest extends TestCase
         $this->assertEquals($except, $result);
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public static function dynamicPlanNapzacDataProvider()
     {
         return [
-            "case 1" => [4, 10, [[3, 10], [6, 210], [4, 130], [2, 57]], 340] 
+            "case 1" => [2, 5, [3, 2], [10, 15], 25],
+            "case 2" => [4, 10, [3, 6, 4, 2], [10, 210, 130, 57], 340] 
         ];
     }
 
@@ -58,10 +73,10 @@ class RecurrenceFormulaTest extends TestCase
      * @test
      * @dataProvider dynamicPlanNapzacDataProvider
      */
-    public function test_dynamicPlanNapzac(int $N, int $W, array $iList, int $except)
+    public function test_dynamicPlanNapzac(int $N, int $W, array $wList, array $vList, int $except)
     {
         $instance = new RecurrenceFormula;
-        $result = $instance->dynamicPlanNapzac($N, $W, $iList);
+        $result = $instance->dynamicPlanNapzac($N, $W, $wList, $vList);
 
         $this->assertEquals($except, $result);
     }
